f8a38ad75673c58a58f48021e0587e2dab387345
jesusd63 <jusus.diazb2003@gmail.com>
2025-10-31 11:39:45 -0600

    Added Projects Section

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,45 @@
+import { Project } from "components/project";
+
 export default async function Page() {
     return (
         <div className="text-center">
             <h1 className="mb-8">Projects</h1>
+            <Project
+                title="C3 Agent to solve Github issues"
+                description="Worked on adapting SWE-Agent so it would be able to solve OOP code problems. It uses
+                a github repository link and issue link as input to produce a patch that can be applied directly to
+                the branch. It's hosted on Azure and Modal."
+                link="https://github.com/Talent-Pentagon/JesusEnriqueDiazBernalRobinsonBours"
+                projectLink="https://link.springer.com/chapter/10.1007/978-3-032-09044-7_9"
+                date="03/25 - 06/25"
+                technologies={["Azure", "Modal", "HuggingFace", "Python", "Unsloth", "Flask"]}
+                img ="/images/PentagonTeam.jpg"
+                alt="The team at a project presentation"
+            />
+            <Project
+                title="Oracle ChatBot in Telegram"
+                description="Worked on a ChatBot with the purpose of increasing productivity 
+                and visibility in developers’ teams at Oracle by letting them manage tasks 
+                Created a Telegram Bot connected to Oracle Cloud Infrastructure Worked 
+                with a team using scrum framework"
+                link="https://github.com/juanjosalco/Oracle_Java_Bot"
+                date="02/24 - 05/24"
+                technologies={["Oracle Cloud Infrastructure", "Java", "Spring", "React", "Docker", "Kubernetes"]}
+                img="/images/Orabot_Pipeline.jpg"
+                alt="Oracle ChatBot Pipeline Diagram"
+            />
+            <Project 
+                title="Mexican FoodBank App (Web and Mobile)"
+                description="Collaborated with the Mexican Food Bank to create solutions 
+                for the distribution and management of their donations, making it faster and easier.
+                Created a mobile app for donors, and a web app for administrators of the bank.
+                Implemented secure 2FA Worked with an agile framework"
+                link="https://github.com/orgs/BAMX-Food-Bank-Application/repositories"
+                date="08/23 - 11/23"
+                technologies={["Google Firebase", "React Native", "React"]}
+                img="/images/BAMXTeam.jpg"
+                alt="Team presenting the Mexican Food Bank App"
+            />
         </div>
     );
 }
--- components/project.jsx
+++ components/project.jsx
@@ -1,7 +1,51 @@
-export function Project({children}) {
+import Image from "next/image";
+
+export function Project({title, description, link, date, technologies, projectLink, img, alt}) {
     return (
         <div>
-            
+            <div className="flex flex-row gap-10 mb-6 text-center justify-center">
+                <h2 className="text-[#D90429]">{title}</h2>
+                <h2 className="">{date}</h2>
+            </div>
+            {img && (
+                <section className="mb-6">
+                    <figure>
+                        <div className="relative overflow-hidden rounded-lg aspect-7/2">
+                            <Image
+                                src={img}
+                                priority
+                                fill={true}
+                                style={{ objectFit: 'contain' }}
+                                sizes="(max-width: 1024px) 100vw, 1024px"
+                                alt={alt}
+                            />
+                        </div>
+                    </figure>
+                </section>
+            )}
+            <p>{description}</p>
+            <h3 className="mt-4 mb-4">
+                Technologies Used:
+            </h3>
+            {technologies && (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {technologies.map((tech) => (
+                        <div key={tech} className=" bg-[var(--color-other)] p-4">
+                        <h3 className="text-[var(--color-black)]">{tech}</h3>
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div className="flex flex-row justify-center gap-8">
+                {link && (
+                    <a href={link} target="_blank" rel="noopener" className="btn mb-8 mt-8">View on Github</a>
+                )}
+                {projectLink && (
+                    <a href={projectLink} target="_blank" rel="noopener" className="btn mb-8 mt-8">View Paper</a>
+                )}
+            </div>
+            {/* Line to mark the end */}
+            <div className="border-t border-[var(--color-primary-content)] border-3  my-4"></div>
         </div>
     );
 
